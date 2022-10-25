9b4a5ea96d83ebdbdfe93767dd055fe93373b2b4
Яков кулешов <y.kuleshov@xway.ru>
2022-10-26 00:57:45 +0300

    update

--- src/elements/p-icon/PIcon.vue
+++ src/elements/p-icon/PIcon.vue
@@ -1,6 +1,6 @@
 <template>
   <svg class="p-icon" xmlns="http://www.w3.org/2000/svg">
-    <use :href="`${iconUrl}#${name}`" xmlns:xlink="http://www.w3.org/1999/xlink" />
+    <use :href="`${iconUrl}#${name}`" :xlink:href="`${iconUrl}#${name}`" xmlns:xlink="http://www.w3.org/1999/xlink" />
   </svg>
 </template>
 
